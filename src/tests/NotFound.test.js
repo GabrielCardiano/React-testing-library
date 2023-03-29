b536c76c4f55a682c596199718eff7427fcf74ac
GabrielCardiano <gabriel.cardiano1@gmail.com>
2023-03-28 23:34:07 -0300

    req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa renderização do componente "NotFound"', () => {
+  test('Testa se a página contém um heading h2', () => {
+    renderWithRouter(<NotFound />);
+
+    const titleH2 = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+
+    expect(titleH2).toBeInTheDocument();
+  });
+  test('Testa se a página renderiza uma imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const SRC_IMG = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const notFoundImg = screen.getByRole(
+      'img',
+      { name: /pikachu crying because the page requested was not found/i },
+    );
+
+    expect(notFoundImg).toBeInTheDocument();
+    expect(notFoundImg).toHaveAttribute('src', SRC_IMG);
+  });
+});
